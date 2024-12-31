5afc52ae6b865aba8aa20c5b7ce71378bc7a8efc
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-12-31 12:56:10 +1000

    Add basic hello world code

---
+++ src/main/java/com/stemcraft/Template.java
@@ -0,0 +1,12 @@
+package com.stemcraft;
+
+import com.stemcraft.STEMCraftPlugin;
+
+public class Template extends STEMCraftPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+		
+		log("Hello, world!");
+    }
+}
